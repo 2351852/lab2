491e6c4d1fe13bf72785c313ec1980dba5b68272
Koshti01 <2351852@students.wits.ac.za>
2023-04-20 10:50:15 +0200

    Fixing

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// })
 
 
-app.get('/about', function(req, res){
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
+// app.get('/about', function(req, res){
+//     res.sendFile(path.join(__dirname, 'views', 'about.html'));
+// });
 
-app.listen(3000)
-console.log('Express server running on port 3000')
+// app.listen(3000)
+// console.log('Express server running on port 3000')
 
-// const mainRouter = require('./mainRoutes')
+// const mainRouter = express.Router()
 
 // mainRouter.get('/', function (req,res) {
 //     res.send('Hello World. Different approach')
@@ -23,8 +23,11 @@ console.log('Express server running on port 3000')
 // mainRouter.get('/about', (req,res) => {
 //     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 // })
+const mainRouter = require('./mainRoutes')
 
-// const port = process.env.PORT || 3000
-// app.listen(port)
-// console.log('Express server running on port', port)
+
+app.use(mainRouter)
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
     
