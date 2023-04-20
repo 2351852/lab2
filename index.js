929544aa0e78502c35690b2ba58c22f9cc085d09
Koshti01 <2351852@students.wits.ac.za>
2023-04-20 10:26:51 +0200

    Simplify

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// })
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
-// app.listen(3000)
-// console.log('Express server running on port 3000')
 
-// app.get('/about', function(req, res){
-//     res.sendFile(path.join(__dirname, 'views', 'about.html'));
-// });
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
 
-const mainRouter = require('./mainRoutes')
+app.listen(3000)
+console.log('Express server running on port 3000')
+
+// const mainRouter = require('./mainRoutes')
 
 // mainRouter.get('/', function (req,res) {
 //     res.send('Hello World. Different approach')
@@ -23,7 +24,7 @@ const mainRouter = require('./mainRoutes')
 //     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 // })
 
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express server running on port', port)
+// const port = process.env.PORT || 3000
+// app.listen(port)
+// console.log('Express server running on port', port)
     
